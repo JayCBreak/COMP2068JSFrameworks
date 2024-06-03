8b5b794b9aa61a184557f5fb71c74332e926d9bc
Jacob Chotenovsky <github@jacobchotenovsky.com>
2024-06-03 16:54:12 -0400

    Added computer selection

--- LAB02/lab2.js
+++ LAB02/lab2.js
@@ -8,12 +8,12 @@
 */
 
 // Importing the prompt package that was installed
-const prompt = require('prompt')
+const prompt = require('prompt');
 
 // Define a schema
-const schema = {
+var schema = {
     properties: {
-        userPrompt: {
+        userSelection: {
             description: 'Choose ROCK, PAPER, or SCISSORS',
             pattern: /^(ROCK|PAPER|SCISSORS)$/i,
             message: 'You have to choose, ROCK, PAPER, or SCISSORS',
@@ -28,8 +28,23 @@ prompt.start();
 
 // Get the user input
 prompt.get(schema, function(err, result) {
+    // Deal with errors
     if(err) {
         return onError(err);
+    } else {
+        // Run the game
+        console.log("The user chose: " + result.userSelection);
+        var seed = Math.random();
+        
+        // Make the human selection
+        if(seed < 0.34) {
+            var computerSelection = 'PAPER';
+        } else if(seed <= 0.67) {
+            var computerSelection = 'SCISSORS';
+        } else {
+            var computerSelection = 'ROCK';
+        }
+        console.log("The computer chose: " + computerSelection);
     }
 })
 
