fd09591e9789fb4e257f2d65736ed016eb9787a4
Jacob Chotenovsky <github@jacobchotenovsky.com>
2024-06-03 16:09:57 -0400

    Setup lab2.js

---
+++ LAB02/lab2.js
@@ -0,0 +1,13 @@
+/*********************************
+ * COMP2068 - Javascript Frameworks
+ * Jacob Chotenovsky
+ * 200545482
+ * June 3rd 2024
+ * 
+ * Create a program that lets a user play rock paper scissors with an AI
+*/
+
+// Importing the prompt package that was installed
+const prompt = require('prompt')
+
+console.log("Hello World")
