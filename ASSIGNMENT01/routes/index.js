634175f00d62ac5cb8a24f9a7ab8700dc11b95a9
Jacob Chotenovsky <github@jacobchotenovsky.com>
2024-06-13 21:44:35 -0400

    Setup each Home, Contact, About, and Project pages

--- ASSIGNMENT01/routes/index.js
+++ ASSIGNMENT01/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// GET the home page.
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+// GET the about page
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me' });
+});
+// GET the projects page
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+// GET the contact page
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Mef' });
 });
 
 module.exports = router;
