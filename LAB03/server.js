05ec1c36b5fa439fc12514f315b8d49dc4e5db11
Jacob Chotenovsky <github@jacobchotenovsky.com>
2024-06-20 16:15:40 -0400

    Setup Lab3

---
+++ LAB03/server.js
@@ -0,0 +1,11 @@
+/*********************************
+ * COMP2068 - Javascript Frameworks
+ * Jacob Chotenovsky
+ * 200545482
+ * June 20th 2024
+ * 
+ * Create a basic calculator in node js with a connect server
+*/
+
+const connect = require('connect');
+const url = require('url');
