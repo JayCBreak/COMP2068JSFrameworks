5264e5f90357adc92958acbf772a7125d7d5ee9f
Jacob Chotenovsky <github@jacobchotenovsky.com>
2024-08-05 13:59:46 -0400

    Added Mob model

--- ASSIGNMENT02/minecraft-mobs-app/routes/index.js
+++ ASSIGNMENT02/minecraft-mobs-app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Minecraft Mobs Database' });
+  res.render('index', { title: 'Minecraft - Mobs Database' });
 });
 
 module.exports = router;
